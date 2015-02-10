1af42afd1f7f5c8aec79e7920d3cb191da6190e5
Elvin Yung <elvin@eventmobi.com>
2015-02-10 06:00:41 -0500

    Replace imports with minified versions in index.html on Gulp build

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,16 +10,19 @@ var gulp = require('gulp'),
   jshint = require('gulp-jshint'),
   notify = require('gulp-notify'),
   autoprefixer = require('gulp-autoprefixer'),
-  minifycss = require('gulp-minify-css');
+  minifycss = require('gulp-minify-css'),
+  htmlreplace = require('gulp-html-replace');;
 
 var paths = {
   js: ['site/js/*.js', '!site/js/vendor/**/*.js'],
-  scss: 'site/scss/style.scss'
+  scss: 'site/scss/style.scss',
+  index: 'site/index.html'
 };
 
 var dests = {
   js: 'dist/js',
-  css: 'dist/css'
+  css: 'dist/css',
+  index: 'dist'
 };
 
 gulp.task('clean', function(cb) {
@@ -41,11 +44,20 @@ gulp.task('scss', function() {
   return gulp.src(paths.scss)
     .pipe(sass({ errLogToConsole: true }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-    .pipe(gulp.dest(dests.css))
     .pipe(rename({suffix: '.min' }))
     .pipe(minifycss())
     .pipe(gulp.dest(dests.css))
     .pipe(notify({ message: 'compiled and minified scss' }));
+});
+
+gulp.task('index', function() {
+  return gulp.src(paths.index)
+    .pipe(htmlreplace({
+      'css': 'css/style.min.css',
+      'js': 'js/site.min.js'
+    }))
+    .pipe(gulp.dest(dests.index))
+    .pipe(notify({ message: 'copied index.html to dist/index.html' }))
 });
 
 gulp.task('watch', function() {
@@ -56,5 +68,5 @@ gulp.task('watch', function() {
 });
 
 gulp.task('default', ['clean'], function() {
-  gulp.start('js', 'scss');
+  gulp.start('js', 'scss', 'index');
 });
